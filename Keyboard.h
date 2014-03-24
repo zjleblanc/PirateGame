aba7e82e6f3ffb7bc081f3ef1a6bafc38aefaecc
Peter <pforbes@nd.edu>
2014-03-23 21:03:55 -0400

    Create Keyboard.h

---
+++ Keyboard.h
@@ -0,0 +1,15 @@
+#include "SDL.h"
+
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+class Keyboard {
+public:
+	Keyboard();
+	void update();
+	bool getState(SDLKey);
+private:
+	Uint8* keys;
+};
+
+#endif
