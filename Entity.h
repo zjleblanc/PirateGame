892817bd4bd0b0bea290cdfabbe126e60da8e6ac
zjleblanc <zleblanc@nd.edu>
2014-04-27 23:25:07 -0400

    Create Entity.h

---
+++ Entity.h
@@ -0,0 +1,23 @@
+#include "SpriteSheet.h"
+#include "Display.h"
+#include <string>
+using namespace std;
+
+#ifndef ENTITY_H
+#define ENTITY_H
+
+class Entity {
+public:
+	Entity(string);
+	virtual void update(Display*) = 0;
+	virtual void render(int, int, SDL_Surface*) = 0;
+	int getX();
+	int getY();
+	void setLocation(int, int);
+protected:
+	SpriteSheet sheet;
+	int x, y;
+	const double g;
+};
+
+#endif
