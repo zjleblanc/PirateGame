b8fe1e3d9215c2062786f8077a54450cea04728f
zjleblanc <zleblanc@nd.edu>
2014-04-30 00:23:34 -0400

    Update Mouse.h

--- Mouse.h
+++ Mouse.h
@@ -7,13 +7,13 @@
 class Mouse {
 public:
 	Mouse();
-	void update();
-	int getX();
-	int getY();
-	int getXtile(Player*);
-	int getYtile(Player*);
-	int buttonPressed();
-	int buttonReleased(int);
+	void update(); // update x,y in terms of pixels
+	int getX(); // return x in pixels
+	int getY(); // return y in pixels
+	int getXtile(Player*); // return x in tiles
+	int getYtile(Player*); // return y in tiles
+	int buttonPressed(); // returns a 1 if left pressed, 3 if right pressed, 0 if neither pressed
+	int buttonReleased(int); // returns if a specific button is released or not
 private:
 	int x, y;
 	SDL_Event mouseEvent;
