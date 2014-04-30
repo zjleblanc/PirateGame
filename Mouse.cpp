264d0ee53dbe17aa067c3244f86308287e01c407
zjleblanc <zleblanc@nd.edu>
2014-04-30 00:21:38 -0400

    Update Mouse.cpp

--- Mouse.cpp
+++ Mouse.cpp
@@ -2,10 +2,10 @@
 #include "Player.h"
 
 Mouse::Mouse() {
-	x = 0, y = 0;
+	x = 0, y = 0; //initialize x and y position to zero
 }
 
-void Mouse::update() {
+void Mouse::update() { //update mouse location
 	if(SDL_PollEvent(&mouseEvent)) {
 		if(mouseEvent.type == SDL_MOUSEMOTION) {
 			x = mouseEvent.motion.x;
@@ -14,14 +14,14 @@ void Mouse::update() {
 	}
 }
 
-int Mouse::buttonPressed(){
+int Mouse::buttonPressed(){ // check for button pressed
 	SDL_PumpEvents();
 	if(SDL_GetMouseState(&x,&y)&SDL_BUTTON(1)) return 1;		//left button pressed
 	else if(SDL_GetMouseState(&x,&y)&SDL_BUTTON(3)) return 3;  //right button pressed
 	else return 0; //no button / middle pressed
 }
 
-int Mouse::buttonReleased(int button){
+int Mouse::buttonReleased(int button){ // check to see if a specific button is released
 	if(button==buttonPressed())
 		return 0;
 	else
@@ -37,10 +37,10 @@ int Mouse::getY() {
 	return y;
 }
 
-int Mouse::getXtile(Player *player){
+int Mouse::getXtile(Player *player){ // return x position in terms of tiles
 		return player->getX()/16 - 20 + Mouse::getX()/16;
 }
 
-int Mouse::getYtile(Player *player){
+int Mouse::getYtile(Player *player){ // return y position in terms of tiles
 		return player->getY()/16 - 15 + Mouse::getY()/16;
 }
