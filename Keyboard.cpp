9fda56f0a53b3f50c2173126e8eeb67b8e791841
zjleblanc <zleblanc@nd.edu>
2014-04-30 00:26:56 -0400

    Update Keyboard.cpp

--- Keyboard.cpp
+++ Keyboard.cpp
@@ -1,13 +1,13 @@
 #include "Keyboard.h"
 
 Keyboard::Keyboard() {
+	keys = SDL_GetKeyState(NULL); //initialize key states
+}
+
+void Keyboard::update() { //update key states for all keys
 	keys = SDL_GetKeyState(NULL);
 }
 
-void Keyboard::update() {
-	keys = SDL_GetKeyState(NULL);
-}
-
-bool Keyboard::getState(SDLKey INDEX) {
+bool Keyboard::getState(SDLKey INDEX) { //return true if INDEX is pressed, 0 if unpressed
 	return keys[INDEX];
 }
