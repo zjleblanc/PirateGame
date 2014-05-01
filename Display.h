35d0a0c27e8c157e5c32f800a606eb746f61463e
Peter <pforbes@nd.edu>
2014-04-30 20:56:41 -0400

    Update Display.h

--- Display.h
+++ Display.h
@@ -1,3 +1,5 @@
+// Handles all objects to be placed on the screen
+
 #include "SpriteSheet.h"
 #include "Sprite.h"
 #include "Area.h"
@@ -12,45 +14,57 @@ using namespace std;
 
 class Display {
 public:
+
 	Display(int, int);
-	int getWidth();
-	int getHeight();
-	void setOffsets(int, int);
+
 	void render(SDL_Surface*);
-	char* getCurrentTile();
-	Sprite* getSprite(int x, int y);
-	void getOffsets();
+	
 	void ctrIncrement();
+
+	Sprite* getSprite(int x, int y);
 	Sprite* getWaterHandle(int);
 	Sprite* getShoalHandle();
+	Sprite* getIceHandle();
+	Sprite* getChestHandle(int);
+
+	Area* getCurrentArea();
+
 	int getXOffset();
 	int getYOffset();
-	void setArea(Area*);
-	Area* getCurrentArea();
-	Sprite* getChestHandle(int);
-	int isOpen(int);
-	void addChest(Chest* chest);
+	int getWidth();
+	int getHeight();
 	int getchestNum();
+	int isOpen(int);
+	
+	void setArea(Area*);
+	void addChest(Chest* chest);
 	void openChest(int);
+	void setOffsets(int, int);
 
 private:
+
 	void renderTile(int, int, Sprite*, SDL_Surface*);
-	int width, height;
-	int xOffset, yOffset;
-	int ctr;
+
 	SpriteSheet terrainSheet;
 	Sprite ice, grass, bush, flower, flower2, flower3, flower4, sand;
 	Sprite water1, water2, fence_front, fence_left, fence_right;
-	Sprite sand_left, sand_right, sand_down, sand_up, estuary, estuary_down;
+	Sprite sand_left, sand_right, sand_down, estuary, estuary_down;
 	Sprite dock, dock_right, dock_left, dock_up, dock_down;
 	Sprite dock_leftup, dock_rightup, dock_leftdown, dock_rightdown;
 	Sprite road, shoal, sand_leftcorner, sand_rightcorner, rroad;
 	Sprite rfence_horiz, rfence_vertical, rfence_corner1, rfence_corner2, rfence_corner3, rfence_corner4;
 	Sprite solidBack;
-	Area* currentArea;
 	Sprite chest_open, chest_closed;
-	int chestNum;
+
+	Area* currentArea;
+
 	vector<Chest> chestArray;
+
+	int chestNum;
+	int width, height;
+	int xOffset, yOffset;
+	int ctr;
+
 };
 
 #endif
