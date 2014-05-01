36021acb4e0c5204b78f52e8ac4ac1a1572e4788
Peter <pforbes@nd.edu>
2014-04-30 20:51:57 -0400

    Update Chest.h

--- Chest.h
+++ Chest.h
@@ -1,18 +1,21 @@
+// Handles the placing of chests on the map, as well as interactions with them
+
 #ifndef CHEST_H
 #define CHEST_H
-
+#include "Mouse.h"
+#include <string>
 using namespace std;
 
 class Chest {
 public:
-	Chest(int, int);
-	int getx();
-	int gety();
-	int isopen();
-	void openChest();
+        Chest(int, int);
+        int getx();
+        int gety();
+        int isOpen();
+        void openChest();
 
 private:
-	int x, y;
-	int open;
+		int x, y;
+        int open;
 };
 #endif
