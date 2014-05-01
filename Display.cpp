8983b9133627c7c0c9d76c628516eeac6cfcdfe2
Peter <pforbes@nd.edu>
2014-04-30 20:54:51 -0400

    Update Display.cpp

--- Display.cpp
+++ Display.cpp
@@ -1,20 +1,21 @@
 #include "Display.h"
 #include <iostream>
 #include <cmath>
-//#include <Windows.h>
 #include <sstream>
 #include <string>
 using namespace std;
 
+// CONSTRUCTOR
+
 Display::Display(int WIDTH, int HEIGHT) 
-	:terrainSheet( "Terrain.png" ),
+	:terrainSheet( "Terrain.png" ),									// main spritesheet to be used for the map display
 
 	ice(terrainSheet.getSurface(), 0, 0, 16, false, true),
 	grass(terrainSheet.getSurface(), 0, 1, 16, false, false),
 	flower(terrainSheet.getSurface(), 1, 1, 16, false, false),
 	flower2(terrainSheet.getSurface(), 1, 3, 16, false, false),
 	flower3(terrainSheet.getSurface(), 2, 3, 16, false, false),
-	flower4(terrainSheet.getSurface(), 2, 4, 16, false, false),
+	flower4(terrainSheet.getSurface(), 2, 4, 16, false, false),		// instantiates (statically) all sprites that are a part of the map
 	bush(terrainSheet.getSurface(), 2, 1, 16, true, false), 
 	sand(terrainSheet.getSurface(), 3, 1, 16, false, false),
 	water1(terrainSheet.getSurface(), 5, 1, 16, true, false),
@@ -25,7 +26,6 @@ Display::Display(int WIDTH, int HEIGHT)
 	sand_left(terrainSheet.getSurface(), 3, 0, 16, false, false),
 	sand_right(terrainSheet.getSurface(), 2, 0, 16, false, false),
 	sand_down(terrainSheet.getSurface(), 5, 0, 16, false, false),
-	sand_up(terrainSheet.getSurface(), 4, 1, 16, false, false),
 	estuary(terrainSheet.getSurface(), 4, 10, 16, false, false),
 	estuary_down(terrainSheet.getSurface(), 4, 11, 16, false, false),
 	dock(terrainSheet.getSurface(), 9, 10, 16, false, false),
@@ -50,7 +50,7 @@ Display::Display(int WIDTH, int HEIGHT)
 	rfence_corner4(terrainSheet.getSurface(), 2, 15, 16, true, false),
 	rroad(terrainSheet.getSurface(), 0, 13, 16, false, false),
 	chest_closed(terrainSheet.getSurface(), 2, 2, 16, true, false),
-    chest_open(terrainSheet.getSurface(), 1, 2, 16, true, false)
+   chest_open(terrainSheet.getSurface(), 1, 2, 16, true, false)
 {
 
 		width = WIDTH;
@@ -58,58 +58,38 @@ Display::Display(int WIDTH, int HEIGHT)
 		xOffset = 0;
 		yOffset = 0;
 		ctr = 0;
-		chestNum = 0;
 
-		for(int i = 0; i <= 100; i++){	
+		for(int i = 0; i <= 100; i++){										// sets up the array of chests to be rendered on the display
 			Chest* chest = new Chest(0,0);
 			chestArray.push_back(*chest);
 		}
 
 }
 
-int Display::getWidth() {
-	return width;
-}
-
-int Display::getHeight() {
-	return height;
-}
-
-void Display::setOffsets(int x, int y) {
-	xOffset = x;
-	yOffset = y;
-}
-
-void Display::renderTile(int x, int y, Sprite* sprite, SDL_Surface* screen) {
-	int xRender = x - xOffset % 16;
-	int yRender = y - yOffset % 16;
-
-	if(xOffset < 0) {
-		xRender = x + (abs(xOffset) % 16);
-	}
-	if(yOffset < 0) {
-		yRender = y + (abs(yOffset) % 16);
-	}
-
-	(*sprite).render(xRender, yRender, screen);
-}
+// PUBLIC METHODS
 
 void Display::render(SDL_Surface* screen) {
 	ctr++;
 	int xTileMin = xOffset >> 4;
 	int yTileMin = yOffset >> 4;
-	int xTileMax = (xOffset + width + 16) >> 4;
+	int xTileMax = (xOffset + width + 16) >> 4;								// corner pins, convert from pixel to tile precision
 	int yTileMax = (yOffset + height + 16) >> 4;
 
 	for(int y = yTileMin; y < yTileMax; y++) {
 		for(int x = xTileMin; x < xTileMax; x++) {
-			renderTile(((x - xTileMin) << 4), ((y - yTileMin) << 4), getSprite(x, y), screen);
-		}
+			renderTile(((x - xTileMin) << 4), ((y - yTileMin) << 4), getSprite(x, y), screen);			// call renderTile for all tiles 
+		}																								// within the bounds of the corner pins
 	}
 }
 
-Sprite* Display::getSprite(int x, int y) {
-	if(x < 0 || y < 0 || x >= currentArea->getWidth() - 1 || y >= currentArea->getHeight() - 1) {
+void Display::ctrIncrement() {								// increments the counter used for rendering dynamic tiles (like water)
+	ctr++;
+}
+
+// GETTERS AND SETTERS
+
+Sprite* Display::getSprite(int x, int y) {					// based on the provided coordinates, gets the char from the current Area's 
+	if(x < 0 || y < 0 || x >= currentArea->getWidth() - 1 || y >= currentArea->getHeight() - 1) {	// map vector and returns the corresponding sprite
 		return &grass;
 	}
 
@@ -162,9 +142,6 @@ Sprite* Display::getSprite(int x, int y) {
 	case 'f':
 		return &sand_rightcorner;
 		break;
-	case 'o':
-		return &sand_up;
-		break;
 	case 'e':
 		return &estuary;
 		break;
@@ -206,7 +183,7 @@ Sprite* Display::getSprite(int x, int y) {
 		}
 		break;
 	case 'r':
-		return &rfence_horiz;
+			return &rfence_horiz;
 		break;
 	case '9':
 		return &rfence_vertical;
@@ -242,77 +219,26 @@ Sprite* Display::getSprite(int x, int y) {
 			return &chest_closed;
 		}
 		break;
-
 	}
 }
 
-char* Display::getCurrentTile() {
-	int xTile = (xOffset + 8 + width / 2) >> 4;
-	int yTile = (yOffset + 8 + height / 2) >> 4;
-
-	char tile = '&';
-
-	if((xTile >= 0) && (yTile >= 0) && (xTile < currentArea->getWidth()) && (yTile < currentArea->getHeight())) {
-		tile = currentArea->getTile(xTile, yTile);
-	}
-
-	switch(tile) {
-	case 'G':
-		return "grass";
-		break;
-	case 'I':
-		return "ice";
-		break;
-	case 'A':
-		return "flower";
-		break;
-	case 'B':
-		return "bush";
-		break;
-	case 'S':
-		return "sand";
-		break;
-	case 'W':
-		return "water";
-		break;
-	default:
-		return "tile not found";
-	}
-}
-
-void Display::ctrIncrement() {
-	ctr++;
-}
-
-Sprite* Display::getWaterHandle(int which) {
-	if(which == 1) {
+Sprite* Display::getWaterHandle(int which) {	// returns a handle to the water tile, used for checking the player/AIs current tile
+	if(which == 1) {							// two possible return values since the tile is dynamic
 		return &water1;
 	} else {
 		return &water2;
 	}
 }
 
-Sprite* Display::getShoalHandle() {
+Sprite* Display::getShoalHandle() {				// returns a handle to the shoal tile, used for checking the player/AI collisions
 	return &shoal;
 }
 
-int Display::getXOffset() {
-	return xOffset;
+Sprite* Display::getIceHandle() {				// returns a handle to the ice tile, used for player sliding
+	return &ice;
 }
 
-int Display::getYOffset() {
-	return yOffset;
-}
-
-void Display::setArea(Area* newArea) {
-	currentArea = newArea;
-}
-
-Area* Display::getCurrentArea() {
-	return currentArea;
-}
-
-Sprite* Display::getChestHandle(int which){
+Sprite* Display::getChestHandle(int which){		// returns a handle to the shoal tile, used for opening chests  
 	if(which){
 		return &chest_closed;
 	} else {
@@ -320,25 +246,69 @@ Sprite* Display::getChestHandle(int which){
 	}
 }
 
-void Display::addChest(Chest* chest){
-	chestNum++;
-	int ypos = chest->gety();
-	chestArray[ypos]= *chest; 
+Area* Display::getCurrentArea() {				// returns the current area the display is rendering
+	return currentArea;
 }
 
-int Display::getchestNum(){
+int Display::getXOffset() {						// return x or y offsets based on the display's current offset relative to the current area
+	return xOffset;
+}
+
+int Display::getYOffset() {
+	return yOffset;
+}
+
+int Display::getWidth() {						// return the height or width of the display window (rendered image)
+	return width;
+}
+
+int Display::getHeight() {
+	return height;
+}
+
+int Display::getchestNum() {					// number of chests in the display
 	return chestNum;
 }
 
-
-int Display::isOpen(int y){
+int Display::isOpen(int y){						// used to determine which chest image to render 
 	if(chestArray[y].isOpen()){
-			return 1;
-		} else{
-			return 0;
-		}
+		return 1;
+	} else{
+		return 0;
+	}
+}
+
+void Display::setOffsets(int x, int y) {
+	xOffset = x;
+	yOffset = y;
+}
+
+void Display::setArea(Area* newArea) {			// used for changing the current area (not implemented)
+	currentArea = newArea;
+}
+
+void Display::addChest(Chest* chest) {			// used for pushing new chests onto the vector
+	chestNum++;
+	int ypos = chest->gety();
+	chestArray[ypos]= *chest;
 }
 
 void Display::openChest(int y){
-			chestArray[y].openChest();
+	chestArray[y].openChest();
+}
+
+// PRIVATE METHODS
+
+void Display::renderTile(int x, int y, Sprite* sprite, SDL_Surface* screen) {		// based on the information provided by render, calls 
+	int xRender = x - xOffset % 16;													// sprite.render to put a tile on a certain screen location
+	int yRender = y - yOffset % 16;
+
+	if(xOffset < 0) {
+		xRender = x + (abs(xOffset) % 16);
+	}
+	if(yOffset < 0) {
+		yRender = y + (abs(yOffset) % 16);
+	}
+
+	(*sprite).render(xRender, yRender, screen);
 }
