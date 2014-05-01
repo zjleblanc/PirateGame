ac14bb6b2f28120c6c3fabf51eae9e3e372c8fa3
Peter <pforbes@nd.edu>
2014-04-30 21:21:41 -0400

    Update TextWriter.cpp

--- TextWriter.cpp
+++ TextWriter.cpp
@@ -2,10 +2,14 @@
 #include <string>
 using namespace std;
 
+// CONSTRUCTOR
+
 TextWriter::TextWriter(int fontSize) {
 	TTF_Init();
-	font = TTF_OpenFont("ARIALN.ttf", fontSize);
+	font = TTF_OpenFont("ARIALN.ttf", fontSize);		// we use an arial narrow font for all text writing
 }
+
+// DECONSTRUCTOR
 
 TextWriter::~TextWriter() {
 	SDL_FreeSurface(text);
@@ -13,8 +17,10 @@ TextWriter::~TextWriter() {
 	TTF_Quit();
 }
 
-void TextWriter::write(char* message, int x, int y, SDL_Surface* screen) {
-	SDL_Color textColor = {0,0,0};
+// PUBLIC METHODS
+
+void TextWriter::write(const char* message, int x, int y, SDL_Surface* screen) {
+	SDL_Color textColor = {0,0,0};								// default color is black
 	text = TTF_RenderText_Solid(font, message, textColor);
 
 	SDL_Rect offset;
@@ -22,5 +28,6 @@ void TextWriter::write(char* message, int x, int y, SDL_Surface* screen) {
 	offset.x = x;
 	offset.y = y;
 
-	SDL_BlitSurface(text, NULL, screen, &offset);
+	SDL_BlitSurface(text, NULL, screen, &offset);				// determines where to render text on the screen
 }
+
