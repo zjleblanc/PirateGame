0ba5736b35fc152c5415b66d15443dff293ba6ad
Peter <pforbes@nd.edu>
2014-04-30 21:14:36 -0400

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,3 +1,5 @@
+// Handles all logic, rendering, and user input for the player's character
+
 #include "SpriteSheet.h"
 #include "Mobile.h"
 #include "Projectile.h"
@@ -14,19 +16,31 @@ using namespace std;
 
 class Player : public Mobile {
 public:
+
 	Player(string);
 	~Player();
+
 	void update(Display*); 
 	void renderAttrib(SDL_Surface*, Display* display);
 	void render(int, int, SDL_Surface*);
+
 	bool isSailing();
+	void addDoubloons(double);
+	void subHealth(int);
+	void restoreHealth(int);
+	void restoreCBs(int);
+	int getHealth();
 	void openChest(Display*, SDL_Surface*);
+	void incrementSinks();
+
 private:
+
 	void processInput(Display* display);
 	void renderLand(int, int, SDL_Surface*);
 	void renderSea(int, int, SDL_Surface*);
 	void toBoat(Display*);
 	void fireProjectile();
+
 	Ship* playerVessel;
 	Projectile* left_projectile;
 	Projectile* right_projectile;
@@ -35,7 +49,7 @@ private:
 	bool shipState;
 	int numProjectiles;
 	int maxHealth, currentHealth;
-	int doubloons;
+	double doubloons;
 	int sinks;
 };
 
