e0b54e340b2a80faed1028b5e41d676b68022124
Peter <pforbes@nd.edu>
2014-04-30 20:50:08 -0400

    Update AudioHandler.h

--- AudioHandler.h
+++ AudioHandler.h
@@ -1,10 +1,12 @@
+// Handles loading, playing, and switching main audio tracks in the game
+
 #include "SDL.h"
 #include "SDL_mixer.h"
 
 #ifndef AUDIOHANDLER_H
 #define AUDIOHANDLER_H
 
-enum track {BOATMUSIC = 1, LANDMUSIC = 2};
+enum track {BOATMUSIC = 1, LANDMUSIC = 2, ICEMUSIC = 3, OPENINGMUSIC = 4, CLOSINGMUSIC = 5};
 
 class AudioHandler {
 public:
@@ -16,6 +18,9 @@ public:
 private:
 	Mix_Music* boatTrack;
 	Mix_Music* islandTrack;
+	Mix_Music* iceTrack;
+	Mix_Music* openingTrack;
+	Mix_Music* closingTrack;
 	track CURRENTTRACK;
 };
 
