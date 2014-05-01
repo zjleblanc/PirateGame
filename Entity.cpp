f633193f90ccd1819b006767b93a32b00da5c423
Peter <pforbes@nd.edu>
2014-04-30 20:58:32 -0400

    Update Entity.cpp

--- Entity.cpp
+++ Entity.cpp
@@ -1,13 +1,19 @@
 #include "Entity.h"
 using namespace std;
 
+// CONSTRUCTOR
+
 Entity::Entity(string pathName)
 	:sheet(pathName),
-	g(-9.8)
+	g(-9.8)								// gravitational constant used for particle/ projectile physics
 {
 	x = 0;
 	y = 0;
 }
+
+// PUBLIC METHODS
+
+// GETTERS AND SETTERS
 
 int Entity::getX() {
 	return x;
